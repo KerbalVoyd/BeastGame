42bb659c9d17b3f2f512dfb0b2912f5afabe00cd
KerbalVoyd <90916270+KerbalVoyd@users.noreply.github.com>
2022-05-03 09:02:26 -0400

    Created the game files

---
+++ src/com/codebind/App.java
@@ -0,0 +1,24 @@
+package com.codebind;
+
+import javax.swing.*;
+
+public class App {
+    private JButton button1;
+    private JPanel panel1;
+    private JTextField textField1;
+
+    public static void main(String[] args){
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        JFrame.setDefaultLookAndFeelDecorated(false);
+        JFrame frame = new JFrame("App"); //Define JFrame
+        frame.setContentPane(new App().panel1); //call panel1 inside fram
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
