9256738f1448b9a3d04f5504a2e15108f20b7db7
KerbalVoyd <90916270+KerbalVoyd@users.noreply.github.com>
2022-05-13 09:35:18 -0400

    Created the game files

--- src/com/codebind/App.java
+++ src/com/codebind/App.java
@@ -1,11 +1,11 @@
 package com.codebind;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
+import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.nio.charset.StandardCharsets;
@@ -18,32 +18,27 @@ public class App {
     private JComboBox comboBox1;
     private JTable baseStats;
     private JButton button1;
+    private JButton button2;
+    private JList list1;
+    private JScrollBar scrollBar1;
     public static BlueDragon PseudoCodeDragon = new BlueDragon("Tom","Water", "None", "Dragon", 1, 1, 1, 1, 1, 1, 1, 1);
 
     public App() {
 
-        String[] columnNames = {"Stat", "Value"};
-
-        String[][] data = {
-                {"Attack", "5"},
-                {"Int", "5"},
-                {"HP", "5"},
-                {"SOMETHING", "5"},
-                {"HAHA", "5"},
-
-        };
-
-        baseStats = new JTable(data, columnNames);
-        baseStats.setPreferredScrollableViewportSize(new Dimension(100, 50));
-        baseStats.setFillsViewportHeight(true);
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null, PseudoCodeDragon.moveList.get(0).attack()); //shows stuff
             }
         });
-
-
+        list1.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (list1.isSelectionEmpty()) {
+                    //put stuff I want to do with the selected array thing
+                }
+            }
+        });
     }
     public static void main(String[] args){
         try {
@@ -52,9 +47,12 @@ public class App {
             throw new RuntimeException(e);
         }
 
+
+
         JFrame.setDefaultLookAndFeelDecorated(false);
         JFrame frame = new JFrame("App"); //Define JFrame
         frame.setContentPane(new App().panel1); //call panel1 inside frame
+        //frame.getContentPane().add()
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -67,23 +65,18 @@ public class App {
         PseudoCodeDragon.moveList.get(0).attack();
         ArrayList<Move> moveIndex = new ArrayList<Move>();
         moveIndex.add(PseudoCodeSlash);
+
     }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
+        //THIS IS FIRE MAKE SURE TO UTILIZE CUSTOM CODE MORE
 
-
-
-
-
-
-        DefaultTableModel tblModel = (DefaultTableModel)baseStats.getModel();
-
-        //String tblStat = tblModel.getValueAt(baseStats.getSelectedRow(), 0).toString();
-        //String tblNum = tblModel.getValueAt(baseStats.getSelectedRow(), 1).toString();
-//
-//
-        //baseStats.setValueAt("tester", 0, 1);
+        String[] test = {"lol", "bruh", "zzz"};
+        list1 = new JList(test); //data has type Object[]
+        list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list1.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        list1.setVisibleRowCount(9);
 
     }
 }
