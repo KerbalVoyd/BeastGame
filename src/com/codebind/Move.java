b2bcfdcb65774c9049666aa379a026a3729a8e14
KerbalVoyd <90916270+KerbalVoyd@users.noreply.github.com>
2022-05-16 09:35:28 -0400

    Created the game files

--- src/com/codebind/Move.java
+++ src/com/codebind/Move.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Move extends MoveAttributes {
 
-    public Move(String name, Boolean attack, boolean buffDebuff, int time, int damage, String damageType, String elementalType, int range, int accuracy) {
+    public Move(String name, boolean attack, boolean buffDebuff, int time, int damage, String damageType, String elementalType, int range, int accuracy) {
         super(name, attack, buffDebuff, time, damage, damageType, elementalType, range, accuracy);
     }
 
